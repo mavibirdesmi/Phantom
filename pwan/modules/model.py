7c3014cb601f3db42dde511c0825097e32eb3b87
mavibirdesmi <mavibirdesmi@gmail.com>
2025-05-07 02:05:08 +0300

    chore :: remove print statements

--- pwan/modules/model.py
+++ pwan/modules/model.py
@@ -50,11 +50,9 @@ def rope_apply(x : torch.Tensor, grid_sizes, freqs : torch.Tensor):
     # freqs shape [1024, C, 2, 2] where C is the embedding dimension
     # x shape [B, L, num_heads, 2*C] where B is the batch size, L is the sequence length, and C is the embedding dimension
     n, c = x.size(2), x.size(3) // 2
-    print(x.shape, freqs.shape)
 
     # split freqs
     freqs = freqs.split([c - 2 * (c // 3), c // 3, c // 3], dim=1)
-    print(freqs[0].shape)
 
     # loop over samples
     output = []
@@ -64,18 +62,15 @@ def rope_apply(x : torch.Tensor, grid_sizes, freqs : torch.Tensor):
         # precompute multipliers
         x_b = x[i, :seq_len]
         x_i = x_b.view(seq_len, n, -1, 2, 1) # seq_len x n x c x 2 x 1
-        print(x_i)
 
         freqs_i = torch.cat([
             freqs[0][:f].view(f, 1, 1, -1).expand(f, h, w, -1), # [f, c//3, 2, 2] -> [f, h, w, 4*c//3]
             freqs[1][:h].view(1, h, 1, -1).expand(f, h, w, -1),
             freqs[2][:w].view(1, 1, w, -1).expand(f, h, w, -1)
         ], dim=-1).reshape(seq_len, 1, -1, 2, 2) # [f, h, w, 4*c] -> [seq_len, 1, c, 2, 2]
-        print(freqs_i.shape)
 
         # apply rotary embedding
         x_i = (x_i * freqs_i).sum(4).flatten(2) # [seq_len, n, c, 2, 2]
-        print(x_i.shape)
         x_i = torch.cat([x_i, x[i, seq_len:]])
 
         # append to collection
