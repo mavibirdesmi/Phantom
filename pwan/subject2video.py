3c425b568872014170e3dfc739bc9961260e74bf
mavibirdesmi <mavibirdesmi@gmail.com>
2025-05-07 17:08:52 +0300

    fix :: use disabler as decorator

--- pwan/subject2video.py
+++ pwan/subject2video.py
@@ -138,11 +138,15 @@ class Phantom_Wan_S2V:
         self.sample_neg_prompt = config.sample_neg_prompt
 
 
+    @torch.compiler.disable()
+    def _convert_image_to_tensor(self, image):
+        return TF.to_tensor(image).sub_(0.5).div_(0.5).to(self.device)
+
+
     def get_vae_latents(self, ref_images, device):
         ref_vae_latents = []
         for ref_image in ref_images:
-            with torch.compiler.disable():
-                ref_image = TF.to_tensor(ref_image).sub_(0.5).div_(0.5).to(self.device)
+            ref_image = self._convert_image_to_tensor(ref_image)
             img_vae_latent = self.vae.encode([ref_image.unsqueeze(1)])
             ref_vae_latents.append(img_vae_latent[0])
                     
